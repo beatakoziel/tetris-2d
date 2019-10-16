f4bd0c85e3a01caab315fe2c26e3173b615f04c7
Beata Kozieł <beatainez.k@gmail.com>
2019-10-17 00:04:04 +0200

    added background to menu

--- Menu.cpp
+++ Menu.cpp
@@ -1,11 +1,59 @@
 #include "pch.h"
 #include "Menu.h"
+#include <iostream>
 
 
 Menu::Menu(float width, float height)
 {
 	loadMenuFont();
-	initializeMenuOptions(width, height);
+	initializeMenuOptions(width, height, activeOptionColor, inactiveOptionColor);
+}
+
+void Menu::drawTextInMenu(sf::RenderWindow &window)
+{
+	for (int i = 0; i < MAX_NUMBER_OF_ITEMS; i++)
+	{
+		window.draw(menu[i]);
+	}
+}
+
+void Menu::moveUp()
+{
+	if (selectedItemIndex - 1 >= 0)
+	{
+		menu[selectedItemIndex].setFillColor(inactiveOptionColor);
+		selectedItemIndex--;
+		menu[selectedItemIndex].setFillColor(activeOptionColor);
+	}
+}
+
+void Menu::moveDown()
+{
+	if (selectedItemIndex + 1 < MAX_NUMBER_OF_ITEMS)
+	{
+		menu[selectedItemIndex].setFillColor(inactiveOptionColor);
+		selectedItemIndex++;
+		menu[selectedItemIndex].setFillColor(activeOptionColor);
+	}
+}
+
+sf::Texture Menu::loadMenuBackground()
+{
+	sf::Texture background_menu;
+	if (!background_menu.loadFromFile("resources/imgs/menu-background.jpg"))
+		sf::err();
+
+	return background_menu;
+}
+
+sf::Sprite Menu::createMenuSprite(sf::Texture & background_menu)
+{
+	sf::Vector2i screenDimensions(1280, 720);
+	sf::Sprite menuSprite;
+	menuSprite.setTexture(background_menu);
+	menuSprite.setScale(1.0f, (float)screenDimensions.y / background_menu.getSize().y);
+
+	return menuSprite;
 }
 
 void Menu::loadMenuFont()
@@ -20,60 +68,28 @@ void Menu::loadMenuFont()
 	}
 }
 
-void Menu::initializeMenuOptions(float width, float height)
+void Menu::initializeMenuOptions(float width, float height, sf::Color activeOptionColor, sf::Color inactiveOptionColor)
 {
 	menu[0].setFont(font);
-	menu[0].setFillColor(sf::Color::Green);
+	menu[0].setFillColor(activeOptionColor);
 	menu[0].setString("Play!");
 	sf::FloatRect menuRect0 = menu[0].getLocalBounds();
 	menu[0].setOrigin(menuRect0.left + menuRect0.width / 2.0f, menuRect0.top + menuRect0.height / 2.0f);
 	menu[0].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
 
 	menu[1].setFont(font);
-	menu[1].setFillColor(sf::Color::White);
+	menu[1].setFillColor(inactiveOptionColor);
 	menu[1].setString("Options");
 	sf::FloatRect menuRect1 = menu[1].getLocalBounds();
 	menu[1].setOrigin(menuRect1.left + menuRect1.width / 2.0f, menuRect1.top + menuRect1.height / 2.0f);
 	menu[1].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
 
 	menu[2].setFont(font);
-	menu[2].setFillColor(sf::Color::White);
+	menu[2].setFillColor(inactiveOptionColor);
 	menu[2].setString("Exit");
 	sf::FloatRect menuRect2 = menu[2].getLocalBounds();
 	menu[2].setOrigin(menuRect2.left + menuRect2.width / 2.0f, menuRect2.top + menuRect2.height / 2.0f);
 	menu[2].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
 
 	selectedItemIndex = 0;
-}
-
-Menu::~Menu()
-{
-}
-
-void Menu::drawTextInMenu(sf::RenderWindow &window)
-{
-	for(int i=0; i < MAX_NUMBER_OF_ITEMS; i++)
-	{
-		window.draw(menu[i]);
-	}
-}
-
-void Menu::moveUp()
-{
-	if (selectedItemIndex - 1 >= 0)
-	{
-		menu[selectedItemIndex].setFillColor(sf::Color::White);
-		selectedItemIndex--;
-		menu[selectedItemIndex].setFillColor(sf::Color::Red);
-	}
-}
-
-void Menu::moveDown()
-{
-	if (selectedItemIndex + 1 < MAX_NUMBER_OF_ITEMS)
-	{
-		menu[selectedItemIndex].setFillColor(sf::Color::White);
-		selectedItemIndex++;
-		menu[selectedItemIndex].setFillColor(sf::Color::Red);
-	}
 }
--- Menu.h
+++ Menu.h
@@ -7,19 +7,23 @@
 class Menu
 {
 public:
+	bool visible = true;
 	Menu(float width, float height);
-	void loadMenuFont();
-	void initializeMenuOptions(float width, float height);
-	~Menu();
-
 	void drawTextInMenu(sf::RenderWindow &window);
 	void moveUp();
 	void moveDown();
 	int getPressedItem() { return selectedItemIndex;  }
+	sf::Texture loadMenuBackground();
+	sf::Sprite createMenuSprite(sf::Texture &background_menu);
 
 private:
+	sf::Text menu[MAX_NUMBER_OF_ITEMS];
 	int selectedItemIndex;
 	sf::Font font;
-	sf::Text menu[MAX_NUMBER_OF_ITEMS];
+	sf::Color activeOptionColor = sf::Color(222, 135, 47, 255);
+	sf::Color inactiveOptionColor = sf::Color::White;
+
+	void loadMenuFont();
+	void initializeMenuOptions(float width, float height, sf::Color activeOptionColor, sf::Color inactiveOptionColor);
 };
 
